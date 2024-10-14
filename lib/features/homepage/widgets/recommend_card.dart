1ca63303db286ddb6a2bcc6ffde8031e362a62af
Dopa91 <code.dopatka@gmail.com>
2024-10-14 18:39:01 +0200

    added GestureDetector

--- lib/features/homepage/widgets/recommend_card.dart
+++ lib/features/homepage/widgets/recommend_card.dart
@@ -18,66 +18,69 @@ class RecommendCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(32),
-      ),
-      color: const Color.fromARGB(255, 116, 98, 162),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                imagePath,
-                height: 164,
-                width: 164,
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+        color: const Color.fromARGB(255, 116, 98, 162),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  imagePath,
+                  height: 164,
+                  width: 164,
+                ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              title,
-              style: const TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.w700),
-            ),
-            Text(
-              subtitle,
-              style: const TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.w300),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  price,
-                  style: const TextStyle(
-                      color: Colors.white, fontWeight: FontWeight.w600),
-                ),
-                const SizedBox(
-                  width: 84,
-                ),
-                Row(
-                  children: [
-                    const Icon(
-                      Icons.favorite_border,
-                      size: 16,
-                      color: Colors.white,
-                    ),
-                    const SizedBox(width: 4),
-                    Text(
-                      iconValue,
-                      style: const TextStyle(
+              const SizedBox(height: 8),
+              Text(
+                title,
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                subtitle,
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.w300),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    price,
+                    style: const TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    width: 84,
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.favorite_border,
+                        size: 16,
                         color: Colors.white,
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                      const SizedBox(width: 4),
+                      Text(
+                        iconValue,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
