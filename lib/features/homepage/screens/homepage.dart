3b8c1bc270cb810b37901d60bdab647072ad8e94
Dopa91 <code.dopatka@gmail.com>
2024-10-14 11:26:22 +0200

    created Homepage

---
+++ lib/features/homepage/screens/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage("assets/images/bg_mainscreen.png"),
+          ),
+        ),
+      ),
+    );
+  }
+}
