e8a43e5d44bd5e1e3e8960288a70a85e3a4eab02
Dopa91 <code.dopatka@gmail.com>
2024-10-14 17:33:09 +0200

    added ListView with Cards

--- lib/features/homepage/screens/homepage.dart
+++ lib/features/homepage/screens/homepage.dart
@@ -36,8 +36,8 @@ class Homepage extends StatelessWidget {
                       fontWeight: FontWeight.w700),
                 ),
                 const HorizontMenuBar(),
-                Expanded(
-                  flex: 6,
+                Flexible(
+                  fit: FlexFit.loose,
                   child: Stack(
                     alignment: Alignment.center,
                     fit: StackFit.expand,
@@ -114,7 +114,7 @@ class Homepage extends StatelessWidget {
                         child: Image.asset("assets/images/Burger_3D.png"),
                       ),
                       const Positioned(
-                        bottom: 290,
+                        top: 310,
                         left: 8,
                         child: Text(
                           "We Recommend",
@@ -127,11 +127,108 @@ class Homepage extends StatelessWidget {
                     ],
                   ),
                 ),
+                SizedBox(
+                  height: 300,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      RecommendCard(
+                        imagePath: "assets/images/cat cupcakes_3D.png",
+                        title: "Mogli's Cup",
+                        subtitle: 'Strawberry ice cream',
+                        price: '¥ 8.99',
+                        iconValue: '200',
+                      ),
+                      RecommendCard(
+                        imagePath: "assets/images/Ice.cream.png",
+                        title: "Balu's Cup",
+                        subtitle: 'Pistachio ice cream',
+                        price: '¥ 8.99',
+                        iconValue: '150',
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class RecommendCard extends StatelessWidget {
+  const RecommendCard({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.subtitle,
+    required this.price,
+    required this.iconValue,
+  });
+
+  final String imagePath;
+  final String title;
+  final String subtitle;
+  final String price;
+  final String iconValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        color: const Color.fromARGB(255, 116, 98, 162),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                imagePath,
+                height: 182,
+                width: 200,
+              ),
+              Text(
+                title,
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                subtitle,
+                style: const TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.w300),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Text(
+                    price,
+                    style: const TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w600),
+                  ),
+                  const SizedBox(
+                    width: 100,
+                  ),
+                  const Icon(
+                    Icons.favorite_border,
+                    size: 16,
+                    color: Colors.white,
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    iconValue,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
