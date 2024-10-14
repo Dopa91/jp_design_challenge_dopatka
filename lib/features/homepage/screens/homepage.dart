c6c3c04b1e80c8a28e75b8a6613f951d086a6d98
Dopa91 <code.dopatka@gmail.com>
2024-10-14 12:50:55 +0200

    deleted MyButton

--- lib/features/homepage/screens/homepage.dart
+++ lib/features/homepage/screens/homepage.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:jp_design_challenge_dopatka/features/shared/my_button.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
+    return Material(
+      child: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
             fit: BoxFit.cover,
             image: AssetImage("assets/images/bg_mainscreen.png"),
+          ),
+        ),
+        child: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Choose Your Favorite",
+                  style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  "Snack",
+                  style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                Row(
+                  children: [],
+                )
+              ],
+            ),
           ),
         ),
       ),
