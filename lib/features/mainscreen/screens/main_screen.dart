609b6095ca5053f4569930f4fc90c1ab770f4d06
Dopa91 <code.dopatka@gmail.com>
2024-10-14 13:07:18 +0200

    cleand up and extracted widget

--- lib/features/mainscreen/screens/main_screen.dart
+++ lib/features/mainscreen/screens/main_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jp_design_challenge_dopatka/features/homepage/screens/homepage.dart';
-import 'package:jp_design_challenge_dopatka/features/shared/my_button.dart';
+import 'package:jp_design_challenge_dopatka/features/mainscreen/widgets/my_blurry_cont.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({
@@ -34,57 +33,9 @@ class MainScreen extends StatelessWidget {
             top: 516,
             child: Image.asset("assets/images/T2.png"),
           ),
-          Positioned(
+          const Positioned(
             bottom: 80,
-            child: Container(
-              height: 220,
-              width: 380,
-              decoration: const BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.all(
-                  Radius.circular(32),
-                ),
-              ),
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 24, bottom: 12),
-                    child: Text(
-                      "Feeling Snackish Today?",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.w800),
-                    ),
-                  ),
-                  const Text(
-                    "Explore Angißs most popular snack selection",
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  ),
-                  const Text(
-                    "and get instantly happy.",
-                    style: TextStyle(color: Colors.white, fontSize: 14),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 24),
-                    child: MyButton(
-                      newText: "Order Now",
-                      nextSite: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const Homepage(),
-                          ),
-                        );
-                      },
-                      icon: null,
-                      buttonWidth: 196,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            child: MyBlurryCont(),
           ),
         ],
       ),
