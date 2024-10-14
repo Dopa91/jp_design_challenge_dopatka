6d185c011e02b15cfb756fd0e6079d74e1617e89
Dopa91 <code.dopatka@gmail.com>
2024-10-14 17:37:22 +0200

    created widget

---
+++ lib/features/homepage/widgets/horizont_card_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:jp_design_challenge_dopatka/features/homepage/widgets/recommend_card.dart';
+
+class HorizontCardView extends StatelessWidget {
+  const HorizontCardView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 300,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: const [
+          RecommendCard(
+            imagePath: "assets/images/cat cupcakes_3D.png",
+            title: "Mogli's Cup",
+            subtitle: 'Strawberry ice cream',
+            price: '¥ 8.99',
+            iconValue: '200',
+          ),
+          RecommendCard(
+            imagePath: "assets/images/Ice.cream.png",
+            title: "Balu's Cup",
+            subtitle: 'Pistachio ice cream',
+            price: '¥ 8.99',
+            iconValue: '150',
+          ),
+        ],
+      ),
+    );
+  }
+}
