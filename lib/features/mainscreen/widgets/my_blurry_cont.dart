0634f2b916c3367cf39cd81ab490f78dd7713be1
Dopa91 <code.dopatka@gmail.com>
2024-10-14 15:35:59 +0200

    added border to BlurryContainer

--- lib/features/mainscreen/widgets/my_blurry_cont.dart
+++ lib/features/mainscreen/widgets/my_blurry_cont.dart
@@ -10,53 +10,58 @@ class MyBlurryCont extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlurryContainer(
-      elevation: 4,
-      blur: 12,
-      height: 220,
-      width: 380,
-      color: Colors.transparent,
-      borderRadius: const BorderRadius.all(
-        Radius.circular(32),
-      ),
-      child: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(top: 24, bottom: 12),
-            child: Text(
-              "Feeling Snackish Today?",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w800),
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: const Color.fromARGB(131, 255, 255, 255)),
+          borderRadius: BorderRadius.circular(32)),
+      child: BlurryContainer(
+        elevation: 4,
+        blur: 12,
+        height: 220,
+        width: 380,
+        color: Colors.transparent,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(32),
+        ),
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 24, bottom: 12),
+              child: Text(
+                "Feeling Snackish Today?",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800),
+              ),
             ),
-          ),
-          const Text(
-            "Explore Angißs most popular snack selection",
-            style: TextStyle(color: Colors.white, fontSize: 14),
-          ),
-          const Text(
-            "and get instantly happy.",
-            style: TextStyle(color: Colors.white, fontSize: 14),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 24),
-            child: MyButton(
-              newText: "Order Now",
-              nextSite: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Homepage(),
-                  ),
-                );
-              },
-              icon: null,
-              buttonWidth: 196,
-              fontSize: 14,
+            const Text(
+              "Explore Angißs most popular snack selection",
+              style: TextStyle(color: Colors.white, fontSize: 14),
             ),
-          ),
-        ],
+            const Text(
+              "and get instantly happy.",
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 24),
+              child: MyButton(
+                newText: "Order Now",
+                nextSite: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Homepage(),
+                    ),
+                  );
+                },
+                icon: null,
+                buttonWidth: 196,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
