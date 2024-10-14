a091ff3ea00a7bfaf46f28876c3f3fe8bbcb1468
Dopa91 <code.dopatka@gmail.com>
2024-10-14 10:12:29 +0200

    added imges and button

--- lib/features/mainscreen/screens/main_screen.dart
+++ lib/features/mainscreen/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jp_design_challenge_dopatka/features/shared/my_button.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({
@@ -18,6 +19,28 @@ class MainScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset("assets/images/chick cupcakes_3D.png"),
+            Image.asset("assets/images/T2.png"),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 256),
+              child: Container(
+                height: 156,
+                width: 256,
+                decoration: const BoxDecoration(color: Colors.black),
+                child: const Column(
+                  children: [
+                    Text("Feeling Snackish Today?"),
+                    Text("Explore Angißs most popular snack slection"),
+                    Text("and get instalnty happy."),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 32, right: 32, bottom: 16, top: 16),
+                      child: MyButton(
+                          newText: "Order Now", nextSite: null, icon: null),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
