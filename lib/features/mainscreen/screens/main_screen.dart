ad267697f783da201a23754b1f9b03b42404adf3
Dopa91 <code.dopatka@gmail.com>
2024-10-14 12:50:38 +0200

    fixed issue and added position

--- lib/features/mainscreen/screens/main_screen.dart
+++ lib/features/mainscreen/screens/main_screen.dart
@@ -17,69 +17,74 @@ class MainScreen extends StatelessWidget {
         ),
       ),
       child: Stack(
+        fit: StackFit.expand,
         alignment: Alignment.center,
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                alignment: Alignment.center,
+          Positioned(
+            right: -100,
+            top: 172,
+            child: Transform.scale(
+              scale: 1.25,
+              child: Image.asset(
+                "assets/images/chick cupcakes_3D.png",
+              ),
+            ),
+          ),
+          Positioned(
+            top: 516,
+            child: Image.asset("assets/images/T2.png"),
+          ),
+          Positioned(
+            bottom: 80,
+            child: Container(
+              height: 220,
+              width: 380,
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(32),
+                ),
+              ),
+              child: Column(
                 children: [
-                  Image.asset("assets/images/chick cupcakes_3D.png"),
-                  Positioned(
-                    top: 320,
-                    child: Image.asset("assets/images/T2.png"),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 24, bottom: 12),
+                    child: Text(
+                      "Feeling Snackish Today?",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                  const Text(
+                    "Explore Angißs most popular snack selection",
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                  const Text(
+                    "and get instantly happy.",
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24),
+                    child: MyButton(
+                      newText: "Order Now",
+                      nextSite: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const Homepage(),
+                          ),
+                        );
+                      },
+                      icon: null,
+                      buttonWidth: 196,
+                      fontSize: 14,
+                    ),
                   ),
                 ],
               ),
-              Container(
-                height: 220,
-                width: 380,
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(32),
-                  ),
-                ),
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.only(top: 24, bottom: 12),
-                      child: Text(
-                        "Feeling Snackish Today?",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.w800),
-                      ),
-                    ),
-                    const Text(
-                      "Explore Angißs most popular snack selection",
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                    const Text(
-                      "and get instantly happy.",
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 80, right: 80, bottom: 16, top: 16),
-                      child: MyButton(
-                          newText: "Order Now",
-                          nextSite: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const Homepage(),
-                              ),
-                            );
-                          },
-                          icon: null),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+            ),
           ),
         ],
       ),
