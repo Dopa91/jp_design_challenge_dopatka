ea921bee1867092d7a46e21ed5edfa6e9fef8b5a
Dopa91 <code.dopatka@gmail.com>
2024-10-14 17:37:34 +0200

    extracted widget

--- lib/features/homepage/screens/homepage.dart
+++ lib/features/homepage/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jp_design_challenge_dopatka/features/homepage/widgets/horizont_card_view.dart';
 import 'package:jp_design_challenge_dopatka/features/homepage/widgets/horizont_menu_bar.dart';
 import 'package:jp_design_challenge_dopatka/features/homepage/widgets/recommend_card.dart';
 import 'package:jp_design_challenge_dopatka/features/homepage/widgets/top_card_view.dart';
@@ -16,50 +17,29 @@ class Homepage extends StatelessWidget {
             image: AssetImage("assets/images/bg_mainscreen.png"),
           ),
         ),
-        child: SafeArea(
+        child: const SafeArea(
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
+                Text(
                   "Choose Your Favorite",
                   style: TextStyle(
                       fontSize: 26,
                       color: Colors.white,
                       fontWeight: FontWeight.w700),
                 ),
-                const Text(
+                Text(
                   "Snack",
                   style: TextStyle(
                       fontSize: 26,
                       color: Colors.white,
                       fontWeight: FontWeight.w700),
                 ),
-                const HorizontMenuBar(),
-                const TopCardView(),
-                SizedBox(
-                  height: 300,
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: const [
-                      RecommendCard(
-                        imagePath: "assets/images/cat cupcakes_3D.png",
-                        title: "Mogli's Cup",
-                        subtitle: 'Strawberry ice cream',
-                        price: '¥ 8.99',
-                        iconValue: '200',
-                      ),
-                      RecommendCard(
-                        imagePath: "assets/images/Ice.cream.png",
-                        title: "Balu's Cup",
-                        subtitle: 'Pistachio ice cream',
-                        price: '¥ 8.99',
-                        iconValue: '150',
-                      ),
-                    ],
-                  ),
-                ),
+                HorizontMenuBar(),
+                TopCardView(),
+                HorizontCardView(),
               ],
             ),
           ),
