8b04ff175d18c8be551af92658f480e4bcc8a540
Dopa91 <code.dopatka@gmail.com>
2024-10-14 21:06:58 +0200

    added widget, for more usebility

--- lib/features/detail_bottom_sheet/widgets/ingredients_icon.dart
+++ lib/features/detail_bottom_sheet/widgets/ingredients_icon.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
 
-class IngredientsIcon extends StatelessWidget {
-  const IngredientsIcon({
+class BottomSheetIconRow extends StatelessWidget {
+  const BottomSheetIconRow({
     super.key,
     required this.icon,
+    required this.iconTwo,
+    required this.iconThree,
+    required this.iconFour,
   });
 
   final IconData icon;
+  final IconData iconTwo;
+  final IconData iconThree;
+  final IconData iconFour;
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Icon(
           icon,
+          color: Colors.white,
+          size: 24,
+        ),
+        Icon(
+          iconTwo,
+          color: Colors.white,
+          size: 24,
+        ),
+        Icon(
+          iconThree,
+          color: Colors.white,
+          size: 24,
+        ),
+        Icon(
+          iconFour,
           color: Colors.white,
           size: 24,
         ),
