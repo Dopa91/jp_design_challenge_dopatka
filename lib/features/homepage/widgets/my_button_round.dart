bf8d77207d6203cb64294e601ed49cd44ab36827
Dopa91 <code.dopatka@gmail.com>
2024-10-15 14:47:54 +0200

    added BlurryContainer

--- lib/features/homepage/widgets/my_button_round.dart
+++ lib/features/homepage/widgets/my_button_round.dart
@@ -1,3 +1,4 @@
+import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:flutter/material.dart';
 
 class MyButtonRound extends StatelessWidget {
@@ -26,43 +27,50 @@ class MyButtonRound extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(4.0),
         child: Container(
-          width: buttonWidth,
-          height: 50,
-          padding: const EdgeInsets.all(16),
           decoration: BoxDecoration(
-            border: Border.all(color: Colors.white),
-            color: const Color.fromARGB(255, 218, 85, 187),
+            border: Border.all(color: const Color.fromARGB(255, 242, 210, 234)),
             borderRadius: BorderRadius.circular(32),
           ),
-          child: Row(
-            children: [
-              const Expanded(
-                child: SizedBox(),
-              ),
-              iconFirst == null
-                  ? const SizedBox()
-                  : Icon(
-                      iconFirst,
-                      color: Colors.white,
-                      size: 16,
-                    ),
-              Text(
-                newText,
-                style: TextStyle(
-                    fontSize: fontSize,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.white),
-              ),
-              icon == null
-                  ? const SizedBox()
-                  : Icon(
-                      icon,
-                      color: Colors.white,
-                    ),
-              const Expanded(
-                child: SizedBox(),
-              ),
-            ],
+          child: BlurryContainer(
+            width: buttonWidth,
+            height: 50,
+            blur: 128,
+            padding: const EdgeInsets.all(16),
+            borderRadius: BorderRadius.circular(32),
+            child: Row(
+              children: [
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                iconFirst == null
+                    ? const SizedBox()
+                    : Icon(
+                        iconFirst,
+                        color: Colors.white,
+                        size: 16,
+                      ),
+                const Expanded(
+                  child: SizedBox(),
+                ),
+                Text(
+                  newText,
+                  style: TextStyle(
+                      fontSize: fontSize,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
+                ),
+                icon == null
+                    ? const SizedBox()
+                    : Icon(
+                        icon,
+                        color: Colors.white,
+                      ),
+                const Expanded(
+                  flex: 2,
+                  child: SizedBox(),
+                ),
+              ],
+            ),
           ),
         ),
       ),
