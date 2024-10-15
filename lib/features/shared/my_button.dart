9f52fd9d7751d02bfc9ea816d23e1022103abb23
Dopa91 <code.dopatka@gmail.com>
2024-10-15 16:26:40 +0200

    added Material to fix Bug

--- lib/features/shared/my_button.dart
+++ lib/features/shared/my_button.dart
@@ -21,58 +21,60 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: nextSite,
-      child: Container(
-        width: buttonWidth,
-        height: buttonHeight,
-        padding: const EdgeInsets.all(16),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: const Color.fromARGB(255, 238, 178, 232),
-          ),
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromARGB(255, 221, 133, 198),
-              spreadRadius: 3,
-              blurRadius: 5,
+    return Material(
+      child: GestureDetector(
+        onTap: nextSite,
+        child: Container(
+          width: buttonWidth,
+          height: buttonHeight,
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: const Color.fromARGB(255, 238, 178, 232),
             ),
-            BoxShadow(
-              color: Color.fromARGB(255, 190, 41, 200),
-              spreadRadius: 1,
-              blurRadius: 3,
-            )
-          ],
-          gradient: const LinearGradient(
-            colors: [
-              Color.fromARGB(255, 218, 85, 187),
-              Color.fromARGB(255, 226, 131, 218),
-              Color.fromARGB(255, 243, 171, 230),
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromARGB(255, 221, 133, 198),
+                spreadRadius: 3,
+                blurRadius: 5,
+              ),
+              BoxShadow(
+                color: Color.fromARGB(255, 190, 41, 200),
+                spreadRadius: 1,
+                blurRadius: 3,
+              )
+            ],
+            gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 218, 85, 187),
+                Color.fromARGB(255, 226, 131, 218),
+                Color.fromARGB(255, 243, 171, 230),
+              ],
+            ),
+            color: const Color.fromARGB(255, 218, 85, 187),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Text(
+                newText,
+                style: TextStyle(
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              const Expanded(child: SizedBox()),
+              icon == null
+                  ? const SizedBox()
+                  : Icon(
+                      icon,
+                      color: Colors.white,
+                    )
             ],
           ),
-          color: const Color.fromARGB(255, 218, 85, 187),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Row(
-          children: [
-            const Expanded(
-              child: SizedBox(),
-            ),
-            Text(
-              newText,
-              style: TextStyle(
-                  fontSize: fontSize,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            const Expanded(child: SizedBox()),
-            icon == null
-                ? const SizedBox()
-                : Icon(
-                    icon,
-                    color: Colors.white,
-                  )
-          ],
         ),
       ),
     );
