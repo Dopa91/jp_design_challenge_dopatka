92d95952837dec6ce447bd9f7d0b82c6e7744534
Dopa91 <code.dopatka@gmail.com>
2024-10-15 14:46:49 +0200

    added Linear Gradient & fixed issue BottomSheet ( hight was only 50%)

--- lib/features/homepage/widgets/recommend_card.dart
+++ lib/features/homepage/widgets/recommend_card.dart
@@ -22,6 +22,7 @@ class RecommendCard extends StatelessWidget {
     return GestureDetector(
       onTap: () {
         showModalBottomSheet(
+          isScrollControlled: true,
           context: context,
           builder: (BuildContext context) {
             return BottomSheetPage(
@@ -29,66 +30,74 @@ class RecommendCard extends StatelessWidget {
           },
         );
       },
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(32),
-        ),
-        color: const Color.fromARGB(255, 116, 98, 162),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Align(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  imagePath,
-                  height: 164,
-                  width: 164,
+      child: Padding(
+        padding: const EdgeInsets.only(right: 28),
+        child: Container(
+          width: 202,
+          decoration: BoxDecoration(
+            border: Border.all(color: const Color.fromARGB(255, 234, 185, 218)),
+            borderRadius: BorderRadius.circular(32),
+            gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 190, 113, 221),
+                Color.fromARGB(255, 117, 119, 233),
+                Color.fromARGB(207, 124, 127, 204),
+              ],
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    imagePath,
+                    height: 164,
+                    width: 164,
+                  ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                title,
-                style: const TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.w700),
-              ),
-              Text(
-                subtitle,
-                style: const TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.w300),
-              ),
-              const SizedBox(height: 8),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    price,
-                    style: const TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.w600),
-                  ),
-                  const SizedBox(
-                    width: 84,
-                  ),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.favorite_border,
-                        size: 16,
-                        color: Colors.white,
-                      ),
-                      const SizedBox(width: 4),
-                      Text(
-                        iconValue,
-                        style: const TextStyle(
+                const SizedBox(height: 8),
+                Text(
+                  title,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  subtitle,
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w300),
+                ),
+                const SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      price,
+                      style: const TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.w600),
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.favorite_border,
+                          size: 16,
                           color: Colors.white,
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                        const SizedBox(width: 4),
+                        Text(
+                          iconValue,
+                          style: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
