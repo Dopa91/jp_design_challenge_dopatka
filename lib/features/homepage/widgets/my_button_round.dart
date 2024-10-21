8ac50879bc405cc97ab9ff8eec0e9e5a6536566f
Dopa91 <code.dopatka@gmail.com>
2024-10-21 09:15:41 +0200

    edited widget RoundButton, with MyBlurryCont

--- lib/features/homepage/widgets/my_button_round.dart
+++ lib/features/homepage/widgets/my_button_round.dart
@@ -1,5 +1,5 @@
-import 'package:blurrycontainer/blurrycontainer.dart';
 import 'package:flutter/material.dart';
+import 'package:jp_design_challenge_dopatka/features/shared/my_blurry_container.dart';
 
 class MyButtonRound extends StatelessWidget {
   const MyButtonRound({
@@ -26,51 +26,42 @@ class MyButtonRound extends StatelessWidget {
       onTap: nextSite,
       child: Padding(
         padding: const EdgeInsets.all(4.0),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: const Color.fromARGB(255, 242, 210, 234)),
-            borderRadius: BorderRadius.circular(32),
-          ),
-          child: BlurryContainer(
-            width: buttonWidth,
-            height: 50,
-            blur: 128,
-            padding: const EdgeInsets.all(16),
-            borderRadius: BorderRadius.circular(32),
-            child: Row(
-              children: [
-                const Expanded(
-                  child: SizedBox(),
-                ),
-                iconFirst == null
-                    ? const SizedBox()
-                    : Icon(
-                        iconFirst,
-                        color: Colors.white,
-                        size: 16,
-                      ),
-                const Expanded(
-                  child: SizedBox(),
-                ),
-                Text(
-                  newText,
-                  style: TextStyle(
-                      fontSize: fontSize,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.white),
-                ),
-                icon == null
-                    ? const SizedBox()
-                    : Icon(
-                        icon,
-                        color: Colors.white,
-                      ),
-                const Expanded(
-                  flex: 2,
-                  child: SizedBox(),
-                ),
-              ],
-            ),
+        child: MyBlurryContainer(
+          width: buttonWidth,
+          height: 50,
+          child: Row(
+            children: [
+              const Expanded(
+                child: SizedBox(),
+              ),
+              iconFirst == null
+                  ? const SizedBox()
+                  : Icon(
+                      iconFirst,
+                      color: Colors.white,
+                      size: 16,
+                    ),
+              const Expanded(
+                child: SizedBox(),
+              ),
+              Text(
+                newText,
+                style: TextStyle(
+                    fontSize: fontSize,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white),
+              ),
+              icon == null
+                  ? const SizedBox()
+                  : Icon(
+                      icon,
+                      color: Colors.white,
+                    ),
+              const Expanded(
+                flex: 2,
+                child: SizedBox(),
+              ),
+            ],
           ),
         ),
       ),
